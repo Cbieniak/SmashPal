3909f4136e0a849c395845c427f5e041b1c59cf0
Christian Bieniak <cbieniak@gmail.com>
2019-10-10 20:11:53 +1100

    Initial Commit

---
+++ SmashPal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmashPal
+//
+//  Created by ChristianBieniak on 10/10/19.
+//  Copyright © 2019 BienThere. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
